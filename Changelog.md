ea5d25003b4062bfc336a7d1152f53f4df189285
riserdpratham <161805472+riserdpratham@users.noreply.github.com>
2024-03-17 02:14:15 +0530

    added the userItem,user and navigation routes and styling (#3)

---
+++ Changelog.md
@@ -0,0 +1,9 @@
+# You must need to add the changes while raising the merge request
+
+## Format should be
+`[version bump up] - Date of PR raised followed by - changes in short`
+
+[0.1.2] 17-March-2023
+- Added the UserItem, UserList componets and styling
+- Added the UIelements of avatar card and backdrop for closing sidedrawer
+- Added the navigation link in header implementation with styling
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,13 +2,15 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USERS =[
-    {id:1,name:'RD',image:'',place:''},
-    {id:2,name:'RD',image:'',place:''}
+  const USERS = [
+    {
+      id: 1,
+      name: "RD",
+      image: "https://media.geeksforgeeks.org/gfg-gg-logo.svg",
+      places: 3,
+    },
   ];
-  return (
-    <UsersList items={USERS}/>
-  );
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
