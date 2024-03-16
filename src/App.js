51a7023e74d921f6cff72cc01b9793be90320f5c
riserdpratham <alchemist.rd23@gmail.com>
2024-03-17 02:10:00 +0530

    added the userItem,user and navigation routes and styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -8,17 +7,20 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" exact element={<Users />} />
-        <Route path="/newPlace" exact element={<NewPlace />} />
-        
-        {/* Redirect all other routes to the root */}
-        <Route path="*" element={<Navigate to="/" />} />{" "}
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" exact element={<Users />} />
+          <Route path="/newPlace" exact element={<NewPlace />} />
+          {/* Redirect all other routes to the root */}
+          <Route path="*" element={<Navigate to="/" />} />{" "}
+        </Routes>
+      </main>
     </Router>
   );
 };
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 import "./UsersList.css";
-import UserItem  from "./UserItem";
 
-const UsersList = props => {
-    if (props.items.length === 0) {
-        return (
-          <div className="center">
-            <h2>No Users Found</h2>
-          </div>
-        );
-      }
-      return (
-        <ul className="center">
-          {props.items.map((user) => (
-            <UserItem
-              key={user.id}
-              id={user.id}
-              image={user.image}
-              name={user.name}
-              placeCount={user.places}
-            />
-          ))}
-        </ul>
-      );
-}
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No Users Found</h2>
+        </Card>
+      </div>
+    );
+  }
+  return (
+    <ul className="center">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
 
-UsersList.propTypes = {}
+UsersList.propTypes = {};
 
-export default UsersList
+export default UsersList;
